8d752503845999fe1e0efbba84793c580ddf085d
Mosesvictor1 <federaluk0@gmail.com>
2026-09-09 23:19:15 +0100

    http urls updated

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       "/news-proxy": {
-        target: "http://167.71.6.117:8080",
+        target: "https://staging.mycampushut.com",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/news-proxy/, "/campusHutNews"),
